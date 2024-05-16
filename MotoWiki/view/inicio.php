9597fac99a9c1f2e0ee1bceca356255e0633224f
davidCIFP01 <200@cifpceuta.es>
2024-05-16 20:25:29 +0200

    CSS INICIO TERMINADO (FALTA MOBILE)

--- MotoWiki/view/inicio.php
+++ MotoWiki/view/inicio.php
@@ -23,55 +23,183 @@
     <div class="moduloHorizontal">
         <h2>Últimas Noticias</h2>
         <hr>
+        
         <div class="contenedorTarjetas">
+            
+            <div class="tarjetaNoticia">
+                <picture class="tarjetaContenedorImagen">
+                    <img src="../view/assets/images/noticia/noticia1.jpeg" alt="MDN" />
+                </picture>
+                <div class="tarjetaConetendorContenido">
+                    <h2> Noticia 1</h2>
+                    <hr>
+                    <p class="pMeGustas">15 <i class="fa-regular fa-thumbs-up"></i></p>
+                    <p class="pAutor">Hecha por: davidrdgz</p>
+                </div>
+            </div>
+
             <div class="tarjetaNoticia">
                 <div class="tarjetaContenedorImagen">
 
                 </div>
                 <div class="tarjetaConetendorContenido">
+                    <h2>Kawasaki saca su nueva motocicleta Ninja 900 2024</h2>
+                    <hr>
+                    <p class="pMeGustas">15 <i class="fa-regular fa-thumbs-up"></i></p>
+                    <p class="pAutor">Hecha por: davidrdgz</p>
+                </div>
+            </div>
 
+            <div class="tarjetaNoticia">
+                <div class="tarjetaContenedorImagen">
+
+                </div>
+                <div class="tarjetaConetendorContenido">
+                    <h2> asio`vusuioabvuioasbvuiabsouivbasu bvoaiusb voasbivbasiv biasb vipasbpiv</h2>
+                    <hr>
+                    <p class="pMeGustas">15 <i class="fa-regular fa-thumbs-up"></i></p>
+                    <p class="pAutor">Hecha por: davidrdgz</p>
                 </div>
             </div>
 
             <div class="tarjetaNoticia">
                 <img>
             </div>
-
-            <div class="tarjetaNoticia">
-                <img>
-            </div>
-
-            <div class="tarjetaNoticia">
-                <img>
-            </div>
             
-            <div class="tarjetaNoticia">
-                <img>
-            </div>
-
-            <div class="tarjetaNoticia">
-                <img>
-            </div>
-            
-            <div class="tarjetaNoticia">
-                <img>
-            </div>
         </div>
     </div>
 
     <div class="moduloHorizontal">
         <h2>Últimas Novedades</h2>
         <hr>
+
+        <div class="contenedorTarjetas">
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <a href="#"><h2>YBR125</h2></a>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+        </div>
+
     </div>
 
     <div class="moduloHorizontal">
-        <h2>Motocicletas más populares</h2>
+        <h2>Más populares</h2>
         <hr>
+
+        <div class="contenedorTarjetas">
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <a href="#"><h2>YBR125</h2></a>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+        </div>
     </div>
 
     <div class="moduloHorizontal">
-        <h2>Motocicletas más Baratas</h2>
+        <h2>Más Baratas</h2>
         <hr>
+
+        <div class="contenedorTarjetas">
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <a href="#"><h2>YBR125</h2></a>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+        </div>
     </div>
     
 </section>
