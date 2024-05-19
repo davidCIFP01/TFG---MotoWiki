d5f371e30ede3f2e38f90e46d93d03aa0ba602d4
davidCIFP01 <200@cifpceuta.es>
2024-05-19 22:09:03 +0200

    Perfil Usuario

---
+++ MotoWiki/view/perfilUsuario.php
@@ -0,0 +1,130 @@
+
+<main class="mainPerfil">
+    
+    <h2>PERFIL DE USUARIO</h2>
+
+    <div class="contenedorTusDatos">
+        <span>TUS DATOS</span>
+
+        <div class="contInfoTusDatos">
+            <span>DATO 1</span>
+            <p>VALOR 1</p>
+        </div>
+
+        <div class="contInfoTusDatos">
+            <span>DATO 1</span>
+            <p>VALOR 1</p>
+        </div>
+
+        <div class="contInfoTusDatos">
+            <span>DATO 1</span>
+            <p>VALOR 1</p>
+        </div>
+
+        <div class="contInfoTusDatos">
+            <span>DATO 1</span>
+            <p>VALOR 1</p>
+        </div>
+
+        <div class="contInfoTusDatos">
+            <span>DATO 1</span>
+            <p>VALOR 1</p>
+        </div>
+
+        <div class="contInfoTusDatos">
+            <span>DATO 1</span>
+            <p>VALOR 1</p>
+        </div>
+
+        <button class="botonAzul">EDITAR DATOS</button>
+    </div>
+
+
+    <section class="moduloHorizontal">
+        <h2>Tus Favoritas</h2>
+        <hr>
+
+        <div class="contenedorTarjetas">
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <a href="#"><h2>YBR125</h2></a>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+            <div class="tarjetaMotoMarca">
+                <div class="contenedorImagenMotoMarca">
+                    <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="fotoMotocicleta">
+                </div>
+                <h2>YBR125</h2>
+            </div>
+
+        </div>
+
+    </section>
+
+</main>
