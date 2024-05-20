1199a5aa397b4ba9a8b5a1d61ec4cc85fc07b5a9
DavidCIFP01 <200@cifpceuta.es>
2024-05-20 15:00:27 +0200

    Estilo Pate gestion Usuarios

--- MotoWiki/view/gestionUsuarios.php
+++ MotoWiki/view/gestionUsuarios.php
@@ -5,4 +5,23 @@
         <i class="fa-solid fa-magnifying-glass"></i>
         <input type="search" placeholder="Busqueda por Fabricante">
     </div>
+
+    <div class="contenedorUsuarios">
+        <div class="tarjetaCambiosUsuario">
+            <span class="spanID">( ID: 654232 )</span>
+            <span class="spanUsername">username 1</span>
+            <select class="">
+            <option value="">Usuario</option>
+                <option value="">Colaborador</option>
+                <option value="">Administrador</option>
+                <option value="">Propietario</option>
+            </select>
+        
+            <div class="contenedorBotonesUsuario">
+                <button class="botonNaranja">VETAR</button>
+                <button class="botonAzul" hidden> ACTIVAR </button>
+                <button class="botonRojo">BORRAR</button>
+            </div>
+        </div>
+    </div>
 </main>
