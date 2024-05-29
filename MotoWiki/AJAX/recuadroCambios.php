762c61736c288db50a9831463f975a031e7b6377
davidCIFP01 <200@cifpceuta.es>
2024-05-30 00:33:50 +0200

    Formulario Hecho, Falta implementar

--- MotoWiki/AJAX/recuadroCambios.php
+++ MotoWiki/AJAX/recuadroCambios.php
@@ -193,8 +193,162 @@ if( isset($tipoCambio) ){
 
     }else if($tipoCambio == "nuevaMoto"){
 
+        $contenedorCambios = '
+        <form id="formularioDatos" >
+            <input type="hidden" name="modoCambios" value="nuevaMoto">
+
+            <div class="contenedorImagenCambio">
+                <img src="../view/assets/images/motocicleta/default_motocicleta.jpg" alt="imagenMoto" id="ImagenActual">
+                <input type="file" name="cambioImagen" id="cambioImagen" hidden>
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">NOMBRE MOTO: </label> <input type="text" name="nombreMoto" id="nombreMoto" placeholder="Nombre Motocicleta" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">TIPO DE MOTO: </label> 
+                <input type="text" name="tipoMoto" id="tipoMoto" placeholder="Tipo de Motocicleta" value="">';  
+
+        $contenedorCambios .= '
+                </input>
+            </div> 
+            
+            <div class="contenedorCambioDatos">
+                <label for="">CILINDRADA: </label> <input type="number" step="1" max="3000" min="1" name="cilindradaMoto" id="cilindradaMoto" placeholder="Cilindrada" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">POTENCIA (CV): </label> <input type="number" step="1" max="3000" min="1" name="potencia1" id="potencia1" placeholder="Cilindrada (CV)" value="">
+            </div>
+            
+            <div class="contenedorCambioDatos">
+                <label for="">POTENCIA (KW): </label> <input type="number" step="1" max="3000" min="1" name="potencia2" id="potencia2" placeholder="Cilindrada (KW)" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">REFRIGERACIÓN: </label> <input type="text" name="refrigeracion" id="refrigeracion" placeholder="Refrigeracion" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Tipo Motor: </label> <input type="text" name="tipoMotor" id="tipoMotor" placeholder="Tipo Motor" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Marchas: </label> <input type="text" name="marchas" id="marchas" placeholder="Marchas" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Transmisión: </label> <input type="text" name="transmision" id="transmision" placeholder="Transmisión" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Capacidad: </label> <input type="number" step="0.01" name="capacidad" id="capacidad" placeholder="Capacidad" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Arranque: </label> <input type="text" name="arranque" id="arranque" placeholder="Arranque" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">TAG: </label> <input type="text" name="tag" id="tag" placeholder="TAG" value="">
+            </div>
+            
+            <div class="contenedorCambioDatos">
+                <label for="">Tipo Carnet: </label> <input type="text" name="tipoCarnet" id="tipoCarnet" placeholder="Tipo Carnet" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Precio Minimo: </label> <input type="number" name="precioMin" id="precioMin" placeholder="Precio Minimo" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Precio Maximo: </label> <input type="number" name="precioMax" id="precioMax" placeholder="Precio Maximo" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Altura: </label> <input type="number" name="altura" id="altura" placeholder="Altura" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Peso: </label> <input type="number" name="peso" id="peso" placeholder="Peso" value="">
+            </div>
+            
+            <div class="contenedorCambioDatos">
+                <label for="">Fabricante: </label> 
+                <select name="idFabricante" class="" id="idFabricante" placeholder="Fabricante">
+                    <option selected hidden value=""></option>
+            ';
+
+            foreach(Fabricante::obtenerTodosFabricantes() as $clave=>$Fabricante){
+
+                $contenedorCambios .= '<option value="'.$Fabricante->__get("idFabricante").'">'.$Fabricante->__get("nombreFabricante").'</option>';
+            }
+
+            $contenedorCambios .='
+                </select>
+            </div>
+            <div class="contenedorCambioDatos">
+                <label for="">FECHA FABRICACIÓN: </label> <input type="number" maxlength="4"  name="fechaFabricacion" id="fechaFabricacion" placeholder="Fecha Fabricacion" value="">
+            </div>
+
+            <div class="contenedorDescripciones">
+                <div class="textareaContainer">
+                    <p>DESCRIPCION 1</p>
+                    <textarea name="descripcionMoto" id="descripcionMoto"></textarea>
+                </div>
+            </div>
+        </form>';
+        $contenedorCambios .= '<button class="botonConfirmar botonesCambios botonAzul" id="botonConfirmarCambios" >Confirmar Cambios</button>';
+
+
     }else if($tipoCambio == "nuevoFabricante"){
 
+        $objFabricante = Fabricante::obtenerFabricantePorId($idRecibido);
+
+        // print_r($objFabricante);
+
+        $contenedorCambios = '
+        <form id="formularioDatos" >
+            <input type="hidden" name="modoCambios" value="fabricante">
+
+            <div class="contenedorImagenCambio">
+                <img src="../view/assets/images/motocicleta/default_motocicleta.jpg"  alt="imagenMoto" id="ImagenActual">
+                <input type="file" name="cambioImagen" id="cambioImagen" hidden>
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Nombre Fabricante: </label> <input type="text" name="nombreFabricante" id="nombreFabricante" placeholder="Nombre Fabricante" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Pais Origen</label> <input type="text" name="paisOrigen" id="paisOrigen" placeholder="Pais Origen" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Fecha Fundada</label> <input type="number" maxlength="4" name="fechaFundada" id="fechaFundada" placeholder="Fecha Fundada" value="">
+            </div>
+
+            <div class="contenedorCambioDatos">
+                <label for="">Sitio Web</label> <input type="text" name="sitioWeb" id="sitioWeb" placeholder="Sitio Web" value="">
+            </div>
+
+            <div class="contenedorDescripciones">
+                <div class="textareaContainer">
+                    <p>DESCRIPCION 1</p>
+                    <textarea name="descripcionFabricante1" id="descripcionFabricante1"></textarea>
+                </div>
+
+                <div class="textareaContainer">
+                    <p>DESCRIPCION 2</p>
+                    <textarea name="descripcionFabricante2" id="descripcionFabricante2"></textarea>
+                </div>
+            </div>
+        </form>';
+
+        $contenedorCambios .= '<button class="botonConfirmar botonesCambios botonAzul"  id="botonConfirmarCambios">Confirmar Cambios</button>';
+
+        
     }
 
     echo $contenedorCambios;
